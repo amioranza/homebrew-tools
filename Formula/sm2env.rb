0e08bc0af8923ee8acb9a38c96239d671120154c
A. Mioranza <amioranza@mdcnet.ninja>
2025-03-05 11:45:13 -0300

    Use a more direct approach to install the binary

--- Formula/sm2env.rb
+++ Formula/sm2env.rb
@@ -1,13 +1,14 @@
 class Sm2env < Formula
   desc "CLI tool to fetch AWS Secrets Manager secrets and save them as .env files"
   homepage "https://github.com/amioranza/sm2env"
-  version "0.1.0"
   url "https://github.com/amioranza/sm2env/releases/download/v0.1.0/sm2env-v0.1.0-x86_64-apple-darwin.tar.gz"
   sha256 "b0e0f08d2678e7a9f2ee11467dc99183aeeabab9d787ce39b6500c164fdc0b5e"
+  version "0.1.0"
   license "MIT"
 
   def install
-    bin.install Dir["v0.1.0/*"].first
+    system "tar", "-xzf", "sm2env-v0.1.0-x86_64-apple-darwin.tar.gz"
+    bin.install "v0.1.0/sm2env"
   end
 
   test do
