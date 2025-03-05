32bccd7cb0ebd106ee4fb7ba05851bd4be3ed2bb
A. Mioranza <amioranza@mdcnet.ninja>
2025-03-05 12:17:33 -0300

    Update sm2env formula to version 0.1.1

--- Formula/sm2env.rb
+++ Formula/sm2env.rb
@@ -1,16 +1,14 @@
 class Sm2env < Formula
   desc "CLI tool to fetch AWS Secrets Manager secrets and save them as .env files"
   homepage "https://github.com/amioranza/sm2env"
-  url "https://github.com/amioranza/sm2env/releases/download/v0.1.0/sm2env-v0.1.0-x86_64-apple-darwin.tar.gz"
-  sha256 "b0e0f08d2678e7a9f2ee11467dc99183aeeabab9d787ce39b6500c164fdc0b5e"
-  version "0.1.0"
+  url "https://github.com/amioranza/sm2env/releases/download/v0.1.1/sm2env-v0.1.1-x86_64-apple-darwin.tar.gz"
+  sha256 "3acc04a0cb71b9c1902ca591c91d06ba2dc131ea15de504949ef015aad355d11"
+  version "0.1.1"
   license "MIT"
 
   def install
-    # Extract the tarball
-    system "tar", "xf", cached_download, "-C", buildpath
-    # Install the binary
-    bin.install "v0.1.0/sm2env"
+    system "tar", "-xzf", "sm2env-v0.1.1-x86_64-apple-darwin.tar.gz"
+    bin.install "v0.1.1/sm2env"
   end
 
   test do
