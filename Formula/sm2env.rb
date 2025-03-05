02bce31f0cbd8b7fa0847bb7df450ed52b152fae
A. Mioranza <amioranza@mdcnet.ninja>
2025-03-05 11:39:21 -0300

    Simplify formula to just install binary

--- Formula/sm2env.rb
+++ Formula/sm2env.rb
@@ -4,9 +4,12 @@ class Sm2env < Formula
   version "0.1.0"
   license "MIT"
   
-  # For now, we only have the x86_64 macOS binary available
-  url "https://github.com/amioranza/sm2env/releases/download/v#{version}/sm2env-v#{version}-x86_64-apple-darwin.tar.gz"
-  sha256 "b0e0f08d2678e7a9f2ee11467dc99183aeeabab9d787ce39b6500c164fdc0b5e"
+  if OS.mac?
+    if Hardware::CPU.intel?
+      url "https://github.com/amioranza/sm2env/releases/download/v#{version}/sm2env-v#{version}-x86_64-apple-darwin.tar.gz"
+      sha256 "b0e0f08d2678e7a9f2ee11467dc99183aeeabab9d787ce39b6500c164fdc0b5e"
+    end
+  end
 
   def install
     bin.install "v#{version}/sm2env"
