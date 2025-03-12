90e0450e85b787b80bb1384c252344fec717f02f
A. Mioranza <amioranza@mdcnet.ninja>
2025-03-12 16:42:06 -0300

    Update sm2env formula to use GitHub release URL

--- Formula/sm2env.rb
+++ Formula/sm2env.rb
@@ -1,7 +1,7 @@
 class Sm2env < Formula
   desc "CLI tool to fetch AWS Secrets Manager secrets and save them as .env files"
   homepage "https://github.com/amioranza/sm2env"
-  url "file:///Users/amioranza/Projects/personal/sm2env/release/sm2env-v0.1.3-x86_64-apple-darwin.tar.gz"
+  url "https://github.com/amioranza/sm2env/releases/download/v0.1.3/sm2env-v0.1.3-x86_64-apple-darwin.tar.gz"
   sha256 "f66b367344f71c7f7d810fc542109c27c9e5cf1b958a396bc4f79a95fadb1dfd"
   version "0.1.3"
   license "MIT"
