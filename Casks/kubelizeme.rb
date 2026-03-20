9d7fe7560d72fe5e3775ef91ff119c5687cbdfe2
A. Mioranza <amioranza@mdcnet.ninja>
2026-03-20 12:37:54 -0300

    chore: update kubelizeme cask to v1.2.1

--- Casks/kubelizeme.rb
+++ Casks/kubelizeme.rb
@@ -1,15 +1,15 @@
 cask "kubelizeme" do
-  version "1.2.0"
-  sha256 "9a15faa467367145eeac7fbdc4a42cb1bb8ee3f5e6e7cf84a8af905c21aad2ae"
+  version "1.2.1"
+  sha256 "45a6f5c91665af88d8a13799dd2bfc8e8722b48de14c8522802b8253e67a2293"
 
-  url "https://releases.mioranza.tech/kubelizeme-v1.2.0.dmg"
+  url "https://releases.mioranza.tech/kubelizeme-v1.2.1.dmg"
   name "Kubelizeme"
   desc "Native desktop Kubernetes manager"
   homepage "https://git.mdcnet.casa/amioranza/kubelizeme"
 
   preflight do
     system_command "/usr/bin/xattr",
-      args: ["-cr", "#{staged_path}/kubelizeme.app"]
+      args: ["-cr", "\#{staged_path}/kubelizeme.app"]
   end
 
   app "kubelizeme.app"
