ddbc14995010562992392ea3e84a788e991f4344
A. Mioranza <amioranza@mdcnet.ninja>
2026-04-08 11:21:59 -0300

    chore: update kubelizeme cask to v1.7.9

--- Casks/kubelizeme.rb
+++ Casks/kubelizeme.rb
@@ -1,8 +1,8 @@
 cask "kubelizeme" do
-  version "1.7.8"
-  sha256 "72b090d56b8de2452b5668069bec6fab7319c9fcf76b46a1a296212a56974bbb"
+  version "1.7.9"
+  sha256 "30a3ad8c274fa39b2b9806b2f96a50635bf9c2ba119e24717be62d2b7f1c4028"
 
-  url "https://releases.mioranza.tech/kubelizeme-v1.7.8.dmg"
+  url "https://releases.mioranza.tech/kubelizeme-v1.7.9.dmg"
   name "Kubelizeme"
   desc "Native desktop Kubernetes manager"
   homepage "https://mioranza.tech/kubelizeme.html"
